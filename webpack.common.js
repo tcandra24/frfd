c4be1b7abfa3f575f50bee9a47c898c8f9e1b3f5
Tito Candra <tcpratama007@gmail.com>
2021-08-30 21:35:11 +0700

    commited revision

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
    entry: "./src/main.js",
    output: {
-       path: path.resolve(__dirname, "dist"),
+       path: path.resolve(__dirname, "docs"),
        filename: "[name].js",
        chunkFilename: '[id].[chunkhash].js'
    },
